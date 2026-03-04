8fbea0a2836c4462a168a82c9606b26898a9543f
pedr0-gomes <gomes.pedro@aluno.ufca.edu.br>
2026-03-04 18:05:23 -0300

    feat(ex11): add estrutura inicial com header

---
+++ ex11.c
@@ -0,0 +1,43 @@
+/*
+ * exNN.c —  Imprimir o caminho até o elemento 
+ *
+ * Lista 16 — Estruturas de Informação
+ * Autor: Pedro Gomes
+ * Data:  04/03/2026
+ *
+ * Enunciado:
+ *   Imprimir todos os elemento no caminho até o elemento k
+ *
+ * Entradas:
+ *   - ponteiro p apontando para a raiz da arvore
+     - inteiro k a ser buscado
+ *
+ * Saída esperada:
+ *   - inteiros contidos nos nos da arvore ate o k desejado
+ *
+ * Hipóteses assumidas:
+ *   - [se o enunciado for ambíguo, registre aqui]
+ *
+ * Pseudocódigo:
+ *   [opcional — escreva antes de implementar]
+ */
+
+#include <stdio.h>
+#include "bst.h"
+
+
+/* TODO: implementar */
+
+/* ---------------------------------------------------------
+ * main — monta a árvore de exemplo e testa a função
+ * --------------------------------------------------------- */
+int main(void) {
+    RegInt *raiz = NULL;
+
+    /* Monte aqui a árvore do enunciado */
+
+    /* Teste aqui a função do exercício */
+
+    bst_liberar_arvore(raiz);
+    return 0;
+}
